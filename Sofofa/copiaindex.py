8b1f3bb8b24e949730f396b294e6e48aa35db8cd
unknown <Administrador@LabF1.LIERBL>
2023-01-18 15:33:07 -0400

    'a'

--- Sofofa/copiaindex.py
+++ Sofofa/copiaindex.py
@@ -1,73 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, session, redirect
+import numpy as np
 import pandas as pd
-from pathlib import Path
 
-this_path = Path(__file__)
 
 app = Flask(__name__)
-ds = pd.read_csv("leype.csv")
-df = pd.read_csv("SUBa.csv")
-db = pd.read_csv("bono.csv")
 
-@app.route('/')
-def home():
-    return render_template(str(
-        this_path.parent / "index.html"    
-    ))
+df = pd.read_csv('leype.csv') 
 
-@app.route('/buscador', methods =["GET", "POST"])
-def BUSCADOR():
-    busca = request.form["busca"]
-    if busca == "subsidios":
-        print ("Bienvenido a la seccion de subsidios")
-        tipo = int(input("Ingrese la region: "))
-        print(buscadorsubsidios(tipo))
-        return render_template('index.html', )
 
-    elif busca == "bono":
-        print("Bienvenido a la seccion bonos")
-        tipo = (input("Ingrese la comuna: "))
-        tipo = tipo.upper()
-        print(sal(tipo))
-        return busca
+@app.route('/', methods=("POST", "GET"))
+def html_table():
 
-    elif busca == "Ley presupuesto":
-        print("Bienvenido a la ley de presupuesto,")
-        tipo = input("Eliga el mes de la ley: ")
-        print(leypresupuesto(tipo))
-
-def buscadorsubsidios(tipo: str):
-        in_1 = df[df['REG'] == tipo]
-        if in_1.empty:
-            return 'Region no encontrada, trata con la primera letra en mayuscula'
-        else:
-            return in_1.to_dict()
-
-def sal(tipo: str):
-    in_2 = db[db['Glosa.Comuna'] == tipo]
-    tipo = tipo.upper
-    if in_2.empty:
-        return 'Comuna no encontrada'
-    else:
-        return in_2.to_dict()
-
-def leypresupuesto(tipo: str):
-    in_3 = ds[ds['Mes']== tipo]
-    if in_3.empty:
-        return 'mes no encontrado, Prueba con la primera letra en mayuscula'
-    else:
-        return in_3.to_dict()
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 
 
-if __name__ =='__main__':
-   app.debug = True
-   app.run()
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- Sofofa/index.py
+++ Sofofa/index.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, request, session, redirect
+import numpy as np
 import pandas as pd
-
 app = Flask(__name__)
 ds = pd.read_csv("leype.csv")
 df = pd.read_csv("SUBa.csv")
@@ -8,47 +8,44 @@ db = pd.read_csv("bono.csv")
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/buscador', methods =["GET", "POST"])
-def BUSCADOR(busca: str):
+
+def BUSCADOR():
  busca = request.form["busca"]
  if busca == "subsidios":
   print ("Bienvenido a la seccion de subsidios")
-  tipo = int(input("Ingrese la region: "))
-  print(buscadorsubsidios(tipo))
-  return render_template('index.html', )
-
+  print(buscadorsubsidios())
+  return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
  elif busca == "bono":
     print("Bienvenido a la seccion bonos")
-    tipo = (input("Ingrese la comuna: "))
-    tipo = tipo.upper()
-    print(sal(tipo))
-    return busca
-
+    print(sal())
+    return render_template('simple.html',  tables=[db.to_html(classes='data')], titles=df.columns.values)
  elif busca == "Ley presupuesto":
     print("Bienvenido a la ley de presupuesto,")
-    tipo = input("Eliga el mes de la ley: ")
-    print(leypresupuesto(tipo))
-def buscadorsubsidios(tipo: str):
-        in_1 = df[df['REG'] == tipo]
+    print (leypresupuesto())
+    
+def buscadorsubsidios():
+        tipo = request.form["tipo"]
+        in_1 = df[df['Región'] == tipo]
         if in_1.empty:
             return 'Region no encontrada, trata con la primera letra en mayuscula'
         else:
-            return in_1.to_dict()
-def sal(tipo: str):
+            return in_1.to_dict
+def sal():
+    tipo = request.form["tipo"]
     in_2 = db[db['Glosa.Comuna'] == tipo]
     tipo = tipo.upper
     if in_2.empty:
         return 'Comuna no encontrada'
     else:
-        return in_2.to_dict()
-def leypresupuesto(tipo: str):
+        return in_2.to_html
+def leypresupuesto():
+    tipo = request.form["tipo"]
     in_3 = ds[ds['Mes']== tipo]
     if in_3.empty:
         return 'mes no encontrado, Prueba con la primera letra en mayuscula'
     else:
      return in_3.to_dict()
-
 if __name__ =='__main__':
    app.debug = True
-   app.run()
+   app.run(host='0.0.0.0')
